5da45d23664f18e996acfe239ca3d4ffe7c90a3b
iYanevGame2Slot <iliya_yanev@game2slot.com>
2025-12-02 06:44:22 +0200

    refactor

--- src/main/java/util/Rotation.java
+++ src/main/java/util/Rotation.java
@@ -12,5 +12,47 @@ public record Rotation(Direction direction, int distance) {
 
         return new Rotation(dir, Integer.parseInt(trimmedLine.substring(1)));
     }
+
+    public static RotationResult calcPositionAndZeroPasses(int currentPosition, Rotation rotation) {
+        boolean left = rotation.direction() == Direction.LEFT;
+        int dist = rotation.distance();
+
+        if (dist == 0) {
+            return new RotationResult(currentPosition, currentPosition == 0 ? 1 : 0);
+        }
+
+        long zeros = dist / 100;  // full laps
+
+        int mod = dist % 100;
+        int pos = currentPosition;
+
+        for (int i = 0; i < mod; i++) {
+            if (left) {
+                pos = (pos - 1 + 100) % 100;
+            } else {
+                pos = addRight(pos, 1);
+            }
+
+            if (pos == 0) zeros++;
+        }
+
+        return new RotationResult(pos, (int) zeros);
+    }
+
+    public static int calcPosition(int currentPosition, Rotation rotation) {
+        return switch (rotation.direction()) {
+            case LEFT -> addLeft(currentPosition, rotation.distance());
+            case RIGHT -> addRight(currentPosition, rotation.distance());
+        };
+    }
+
+    private static int addLeft(int currentPosition, int distance) {
+        int newDistance = currentPosition - distance;
+        return ((newDistance % 100) + 100) % 100;
+    }
+
+    private static int addRight(int currentPosition, int distance) {
+        return (currentPosition + distance) % 100;
+    }
 }
 
